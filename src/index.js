8ed897d102169cfc49c04155e6562f7e963fe743
Emanoel Santana <76492419+EmanoelSantana@users.noreply.github.com>
2021-12-22 17:44:23 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Providers from './providers';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Providers />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
